d6f38ce38a2cb49de74a62581f039f1f00f050e4
Shivam <shivamsagar6192@gmail.com>
2026-08-02 22:30:22 +0530

    LeetCode Questions

---
+++ Strings/SB.java
@@ -0,0 +1,17 @@
+public class SB {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i< 26; i++){
+            char ch = (char)('a' + i);
+            builder.append(ch);
+        }
+
+        System.out.println(builder);
+        builder.reverse();
+        System.out.println(builder);
+        builder.deleteCharAt(0);
+        System.out.println(builder);
+        builder.delete(5,9);
+        System.out.println(builder);
+    }
+}
