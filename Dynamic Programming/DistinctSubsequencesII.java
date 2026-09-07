d8dd328916df25d4424f3c67282e5781e9e2e5f6
Shivam <shivamsagar6192@gmail.com>
2026-09-07 12:52:42 +0530

    LeetCode Questions

---
+++ Dynamic Programming/DistinctSubsequencesII.java
@@ -0,0 +1,22 @@
+public class DistinctSubsequencesII {
+    public static int distinctSubseqII(String s) {
+        int MOD = 1_000_000_007;
+
+        int total = 0;
+        int[] end = new int[26];
+
+        for(char ch : s.toCharArray()){
+            int index = ch - 'a';
+            int oldTotal = total;
+            int newSubsequences = (oldTotal + 1 - end[index] + MOD) % MOD;
+            total = (total + newSubsequences) % MOD;
+            end[index] = (end[index] + newSubsequences) % MOD;
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+        System.out.println(distinctSubseqII(s));
+    }
+}
