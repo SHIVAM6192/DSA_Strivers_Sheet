fadd05c2222bcc7d0ff2688c49f8bbae72311cbc
Shivam <shivamsagar6192@gmail.com>
2026-09-16 10:32:57 +0530

    LeetCode Questions

---
+++ Daily-Question/NumberOfSetsOfKNonOverlappingLineSegments.java
@@ -0,0 +1,24 @@
+public class NumberOfSetsOfKNonOverlappingLineSegments {
+    static final long MOD = 1_000_000_007;
+    public static int numberOfSets(int n, int k) {
+        int N = n + k - 1;
+        int R = 2 * k;
+
+        long[] dp = new long[R + 1];
+        dp[0] = 1;
+
+        for(int i=1; i<=N; i++){
+            for(int j=Math.min(i,R); j>=1; j--){
+                dp[j] = (dp[j] + dp[j-1]) % MOD;
+            }
+        }
+
+        return (int) dp[R];
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        int k = 2;
+        System.out.println(numberOfSets(n, k));
+    }
+}
